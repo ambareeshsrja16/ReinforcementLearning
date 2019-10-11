65bd6e06cbe9a5ba33ccf90b5595f71648d5b84d
Ambareesh <ambareeshsrja16@gmail.com>
2019-10-11 15:49:23 -0700

    Updated create_trajectory, get_torque, get_samples_from_trajectory, getIK
    Updated create_trajectory, get_torque, get_samples_from_trajectory, getIK

--- two_dof_arm.py
+++ two_dof_arm.py
@@ -16,7 +16,6 @@ def getForwardModel(q0, q1, l0 = 0.1, l1 = 0.11 ):
 
     state = np.array([pos_x, pos_y, theta_z]).reshape(-1, 1)
     assert state.shape == (3,1)
-
     return state
 
 
@@ -37,4 +36,104 @@ def getJacobian(q0, q1, l0=0.1, l1=0.11):
 
     return jacobian
 
+
+def getIK(x,y, q0_current= 0, q1_current=0):
+    """
+    Inverse kinematics using Jacobian Inverse
+
+    :param x: Position x
+    :param y: Position y
+    :return: Joint angles
+    """
+    assert all(isinstance(i, (int, float)) for i in (x, y)), "Positions have to be floats or integers"
+    assert all(isinstance(i, (int, float)) for i in (q0_current, q1_current)), "Angles have to be floats or integers"
+
+    inverse_jacobian = np.linalg.pinv(getJacobian(q0_current,q1_current))
+    assert inverse_jacobian.shape == (2,3)
+
+    current_position = np.array([x,y,0]).reshape(-1,1)
+
+    del_q = inverse_jacobian@current_position
+    assert del_q.shape == (2,1)
+
+    return del_q
+
+
+def get_samples_from_trajectory(steps=100):
+    """
+    Create x_desired, y_desired from the trajectory equation
+    Pack into numpy array and return
+    :param steps: Number of sample points needed from trajectory
+    :return: Desired trajectory : numpy array of shape (2,n) : First row refers to x, Second row refers to y
+    """
+
+    assert isinstance(steps, int), "Steps has to be integer"
+
+    desired_traj = np.zeros(shape=(2, steps), dtype=float)
+    theta_values = np.linspace(start=-np.pi, stop=np.pi, num=steps)
+
+    c1, c2 = 0.19, 0.02
+    desired_traj[0, :] = (c1 + c2*np.cos(4*theta_values))*np.cos(theta_values)
+    desired_traj[1, :] = (c1 + c2*np.cos(4*theta_values))*np.sin(theta_values)
+
+    return desired_traj
+
+
+def create_trajectory(steps=100, kp_1=1.0, kp_2=1.0, kd_1=1.0, kd_2=1.0, episodes=1):
+    """
+    Given steps, Proportion Control(kp_1, kp_2) and Differential Control parameters (kd_1, kd_2) return observed trajectory (x_obs, y_obs)
+    :param episodes:
+    :param steps:
+    :param kp_1:
+    :param kp_2:
+    :param kd_1:
+    :param kd_2:
+    :return:
+    """
+    assert isinstance(steps, int), "steps has to be integer"
+    assert all(isinstance(i, (float, int)) for i in (kp_1, kp_2, kd_1, kd_2)),"PD controller gains should be integer, float"
+
+    import gym
+    import pybulletgym.envs
+    env = gym.make("ReacherPyBulletEnv-v0")
+    q0_curr = 0  # Initial position
+    q1_curr = 0
+
+    for curr_episode in range(episodes):  # For multiple episodes, Default: episodes= 1
+        env.unwrapped.robot.central_joint.reset_position(q0_curr, 0)
+        env.unwrapped.robot.elbow_joint.reset_position(q1_curr, 0)
+        for robo_step in range(steps):
+            env.render()  # WHY HERE?
+            q0_obs, q0_dot = env.unwrapped.robot.central_joint.current_position()  # Current Observation from Sensor
+            q1_obs, q1_dot = env.unwrapped.robot.elbow_joint.current_position()
+
+            print("\nJoint 1", q0_obs)
+            print("Joint 2", q1_obs)
+
+            #action = env.action_space.sample() #[0.5, 0.7] Sample action. Torque for q0, q1
+            action = get_torque(q0_obs, q1_obs, kp_1, kp_2, kd_1, kd_2)
+            _, _, done, _ = env.step(action)
+            if done:
+                print(f"Episode finished after {robo_step} steps")
+                break
+    env.close()
+
+
+def get_torque(q0_obs, q1_obs, *args):
+    """
+    Given Current observation and PD controller terms, return Torque (action for env action space)
+    :param q0_obs:
+    :param q1_obs:
+    :param args:
+    :return:
+    """
+
+    kp_1, kp_2, kd_1, kd_2 = args
+    assert all(isinstance(i, (float, int)) for i in (kp_1, kp_2, kd_1, kd_2)), "PD controller gains should be integer, float"
+
+    tau_1 = 0.0
+    tau_2 = 0.0
+    action = np.array([tau_1, tau_2], dtype=float)
+
+    return action
 
