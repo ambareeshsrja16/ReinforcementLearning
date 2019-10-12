9d089d758e075e015e92fbd8d8aef0a4588fc437
Ambareesh <ambareeshsrja16@gmail.com>
2019-10-11 22:19:19 -0700

    updated plot_trajectory

--- two_dof_arm.py
+++ two_dof_arm.py
@@ -6,17 +6,22 @@ def getForwardModel(q0, q1,
                     l0=0.1, l1=0.11):
     """
     Takes joint states(angles in radians) and returns end effector pose (3,1) numpy array
+    :param q0:
+    :param q1:
+    :param l0:
+    :param l1:
+    :return:
     """
-
     assert all(isinstance(i, (int, float))for i in (q0, q1)), "Angles have to be floats or integers"
-    assert all(-math.pi <= i <= math.pi for i in (q0, q1)), "Angle should be radians between -Pi and +Pi"
+    #assert all(-math.pi <= i <= math.pi for i in (q0, q1)), "Angle should be radians between -Pi and +Pi"
 
     pos_x = l1 * np.cos(q0+q1) + l0*np.cos(q0)
     pos_y = l1 * np.sin(q0+q1) + l0*np.sin(q0)
     theta_z = q0+q1
 
-    state = np.array([pos_x, pos_y, theta_z]).reshape(-1, 1)
-    assert state.shape == (3,1)
+    state = np.array([pos_x, pos_y, theta_z])
+    assert state.shape == (3,)
+
     return state
 
 
@@ -24,12 +29,16 @@ def getJacobian(q0, q1,
                 l0=0.1, l1=0.11):
     """
     Takes joint states(angles in radians) and returns Jacobian (3,2) np array
+    :param q0:
+    :param q1:
+    :param l0:
+    :param l1:
+    :return:
     """
-
     assert all(isinstance(i, (int, float)) for i in (q0, q1)), "Angles have to be floats or integers"
-    assert all(-math.pi <= i <= math.pi for i in (q0, q1)), "Angle should be radians between -Pi and +Pi"
+    #assert all(-np.pi <= i <= np.pi for i in (q0, q1)), f" {q0, q1} Angles should be radians between -Pi and +Pi"
 
-    jacobian = np.ones(shape=(3,2), dtype=float)
+    jacobian = np.ones(shape=(2,2), dtype=float)
 
     jacobian[0][0] = -(l1 * np.sin(q0+q1) + l0*np.sin(q0))
     jacobian[0][1] = -l1*np.sin(q0+q1)
@@ -62,84 +71,6 @@ def getIK(x, y,
     assert del_q.shape == (2,1)
 
     return del_q
-
-
-def get_samples_from_trajectory(steps=100):
-    """
-    Create x_desired, y_desired from the trajectory equation
-    Pack into numpy array and return
-    :param steps: Number of sample points needed from trajectory
-    :return: Desired trajectory : numpy array of shape (2,n) : First row refers to x, Second row refers to y
-    """
-
-    assert isinstance(steps, int), "Steps has to be integer"
-
-    desired_traj = np.zeros(shape=(2, steps), dtype=float)
-    theta_values = np.linspace(start=-np.pi, stop=np.pi, num=steps)
-
-    c1, c2 = 0.19, 0.02
-    desired_traj[0, :] = (c1 + c2*np.cos(4*theta_values))*np.cos(theta_values)
-    desired_traj[1, :] = (c1 + c2*np.cos(4*theta_values))*np.sin(theta_values)
-
-    return desired_traj
-
-
-def create_trajectory(steps=100,
-                      kp_1=1.0, kp_2=1.0, kd_1=1.0, kd_2=1.0,
-                      q0_curr=-np.pi, q1_curr = np.pi,
-                      episodes=1):
-    """
-    Given steps, Proportion Control(kp_1, kp_2) and Differential Control parameters (kd_1, kd_2) return observed trajectory (x_obs, y_obs)
-    :param q0_curr: Starting position Joint 0
-    :param q1_curr: Starting position Joint 1
-    :param episodes: Number of sequences (one sequence is, one complete lap along the trajectory)
-    :param steps: Number of discretized steps
-    :param kp_1: Proportion Control [0][0]
-    :param kp_2: Proportion Control [1][1]
-    :param kd_1: Derivative Control [0][0]
-    :param kd_2: Derivative Control [1][1]
-    :return: obs_trajectory : (2, steps) numpy array
-    """
-    assert isinstance(steps, int), "steps has to be integer"
-    assert all(isinstance(i, (float, int)) for i in (kp_1, kp_2, kd_1, kd_2)),"PD controller gains should be integer, float"
-
-    import gym
-    import pybulletgym.envs
-    env = gym.make("ReacherPyBulletEnv-v0")
-
-    desired_traj = get_samples_from_trajectory(steps)
-    obs_traj = np.zeros(shape=(2,steps), dtype=float)
-
-    for curr_episode in range(episodes):  # For multiple episodes, Default: episodes= 1
-        env.unwrapped.robot.central_joint.reset_position(q0_curr, 0)
-        env.unwrapped.robot.elbow_joint.reset_position(q1_curr, 0)
-        for robo_step in range(steps):
-            env.render()  # WHY HERE?
-            q0_obs, q0_dot_obs = env.unwrapped.robot.central_joint.current_position()  # Current Observation from Sensor
-            q1_obs, q1_dot_obs = env.unwrapped.robot.elbow_joint.current_position()
-
-            obs_traj[0, robo_step] = q0_obs  # Current trajectory x
-            obs_traj[1, robo_step] = q1_obs  # Current trajectory y
-
-            print("\nJoint 1", q0_obs)
-            print("Joint 2", q1_obs)
-
-            vx_ref, vy_ref = 0, 0
-            x_desired = desired_traj[0,robo_step]
-            y_desired = desired_traj[1, robo_step]
-            # action = env.action_space.sample() #[0.5, 0.7] Sample action. Torque for q0, q1
-            action = get_torque(q0_obs, q1_obs, q0_dot_obs, q1_dot_obs,
-                                x_desired, y_desired, vx_ref, vy_ref,
-                                kp_1, kp_2, kd_1, kd_2)
-            _, _, done, _ = env.step(action) # Provide Torque to Robot
-
-
-            if done:
-                print(f"Episode finished after {robo_step} steps")
-                break
-    env.close()
-
-    return obs_traj
 
 
 def get_torque(q0_obs, q1_obs,
@@ -180,8 +111,107 @@ def get_torque(q0_obs, q1_obs,
     assert e.shape == (2,1) and V_e.shape == (2,1) , f"{e.shape}, {V_e.shape}"
 
     force_end_eff = KP @ e + KD @ V_e  # PD Controller
-    joint_torques = jacobian.T @ force_end_eff  # Torque= (J)^T @ F
+    joint_torques = (jacobian.T @ force_end_eff).reshape(-1)  # Torque= (J)^T @ F
 
     assert joint_torques.shape == (2,), f"{joint_torques.shape}"
     return joint_torques
 
+
+def get_samples_from_trajectory(steps=100):
+    """
+    Create x_desired, y_desired from the trajectory equation
+    Pack into numpy array and return
+    :param steps: Number of sample points needed from trajectory
+    :return: Desired trajectory : numpy array of shape (2,n) : First row refers to x, Second row refers to y
+    """
+
+    assert isinstance(steps, int), "Steps has to be integer"
+
+    desired_traj = np.zeros(shape=(2, steps), dtype=float)
+    theta_values = np.linspace(start=-np.pi, stop=np.pi, num=steps)
+
+    c1, c2 = 0.19, 0.02
+    desired_traj[0, :] = (c1 + c2*np.cos(4*theta_values))*np.cos(theta_values)
+    desired_traj[1, :] = (c1 + c2*np.cos(4*theta_values))*np.sin(theta_values)
+
+    return desired_traj
+
+
+def create_trajectory(steps=100,
+                      kp_1=1.0, kp_2=1.0, kd_1=1.0, kd_2=1.0,
+                      q0_curr=-np.pi, q1_curr=-np.pi,
+                      episodes=1):
+    """
+    Given steps, Proportion Control(kp_1, kp_2) and Differential Control parameters (kd_1, kd_2) return observed trajectory (x_obs, y_obs)
+    :param q0_curr: Starting position Joint 0
+    :param q1_curr: Starting position Joint 1
+    :param episodes: Number of sequences (one sequence is, one complete lap along the trajectory)
+    :param steps: Number of discretized steps
+    :param kp_1: Proportion Control [0][0]
+    :param kp_2: Proportion Control [1][1]
+    :param kd_1: Derivative Control [0][0]
+    :param kd_2: Derivative Control [1][1]
+    :return: obs_trajectory : (2, steps) numpy array
+    """
+    assert isinstance(steps, int), "steps has to be integer"
+    assert all(isinstance(i, (float, int)) for i in (kp_1, kp_2, kd_1, kd_2)),"PD controller gains should be integer, float"
+
+    import gym
+    import pybulletgym.envs
+    env = gym.make("ReacherPyBulletEnv-v0")
+
+    env.render()
+    env.reset()
+    desired_traj = get_samples_from_trajectory(steps)
+    obs_traj = np.zeros(shape=(2, steps), dtype=float)
+
+    for curr_episode in range(episodes):  # For multiple episodes, Default: episodes= 1
+        env.unwrapped.robot.central_joint.reset_position(q0_curr, 0)
+        env.unwrapped.robot.elbow_joint.reset_position(q1_curr, 0)
+        for robo_step in range(steps):
+            q0_obs, q0_dot_obs = env.unwrapped.robot.central_joint.current_position()  # Current Observation from Sensor
+            q1_obs, q1_dot_obs = env.unwrapped.robot.elbow_joint.current_position()
+            obs_traj[:, robo_step] = getForwardModel(q0_obs,q1_obs)[:2]   # Current trajectory x
+
+            if robo_step == 0 or robo_step == steps-1:
+                print("\nJoint 1", q0_obs)
+                print("Joint 2", q1_obs)
+
+            vx_ref, vy_ref = 0, 0
+            x_desired = desired_traj[0,robo_step]
+            y_desired = desired_traj[1, robo_step]
+            # action = env.action_space.sample() #[0.5, 0.7] Sample action. Torque for q0, q1
+            action = get_torque(q0_obs, q1_obs, q0_dot_obs, q1_dot_obs,
+                                x_desired, y_desired, vx_ref, vy_ref,
+                                kp_1, kp_2, kd_1, kd_2)
+            _, _, done, _ = env.step(action) # Provide Torque to Robot
+
+            if done:
+                print(f"Episode finished after {robo_step} steps")
+                break
+    env.close()
+
+    return obs_traj
+
+
+def plot_trajectory(desired_traj, final_trajectory):
+    """
+    Plot Trajectory
+    :param final_trajectory:  Trajectory to be plotted, numpy array (2, steps)
+    :param desired_traj:
+    :return: None
+    """
+    assert isinstance(desired_traj, np.ndarray) and desired_traj.shape[0] == 2
+    assert isinstance(final_trajectory, np.ndarray) and final_trajectory.shape[0] == 2
+
+    import matplotlib.pyplot as plt
+    plt.plot(desired_trajectory[0, :], desired_trajectory[1, :], "r-", linewidth=2)
+    plt.plot(final_trajectory[0, :], final_trajectory[1, :], "g-", linewidth=2)
+    plt.show()
+
+
+if __name__ == "__main__":
+    desired_trajectory = get_samples_from_trajectory(steps=100)
+    final_trajectory = create_trajectory(steps=100)
+    import pdb; pdb.set_trace()
+    plot_trajectory(desired_trajectory, final_trajectory)
